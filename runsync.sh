26cdf2debf2873787c65ac13c7288990e2ac0b3a
Rick164 <RickDB@outlook.com>
2021-01-31 11:30:50 +0100

    Update runsync.sh

--- runsync.sh
+++ runsync.sh
@@ -8,10 +8,10 @@ if [ ! -z "${CUSTOM_MAPPINGS}" ]
 then
   if test -f "${CUSTOM_MAPPINGS}"; then
     echo "Found local custom mapping file and importing: ${CUSTOM_MAPPINGS}"
-    cp ${CUSTOM_MAPPINGS} /plexanisync/custom_mappings.ini
+    cp ${CUSTOM_MAPPINGS} /plexanisync/custom_mappings.yaml
   else
     echo "Found remote custom mapping file and importing: ${CUSTOM_MAPPINGS}"
-    wget -O /plexanisync/custom_mappings.ini ${CUSTOM_MAPPINGS}
+    wget -O /plexanisync/custom_mappings.yaml ${CUSTOM_MAPPINGS}
   fi
 else
   echo "No custom mapping specified"
